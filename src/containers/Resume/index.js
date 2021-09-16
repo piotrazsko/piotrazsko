1f838e2f82be312e779823af907090774c84de1d
Serj <serj432@yandex.ru>
2021-09-16 18:34:33 +0300

    added info

--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -4,55 +4,22 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import { Pane, List } from "components";
 import { useTranslation } from "react-i18next";
-
+import { itemsEducation, itemsWorking } from "config/resume";
 const Resume = ({ history, ...props }) => {
   const { t } = useTranslation();
-  const itemsWorking = [
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-  ];
-  const itemsEducation = [
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-    {
-      title: t("position_0"),
-      text: t("position_0_extend"),
-      dates: t("position_0_dates"),
-    },
-  ];
+
   return (
     <>
       <Pane verticalOffset={26} title={t("resume")}>
         <Grid container>
           <Grid item xs={6}>
             <Box>
-              <List title={t("resume_working")} items={itemsWorking} />
+              <List title={t("resume_working")} items={itemsWorking(t)} />
             </Box>
           </Grid>
           <Grid item xs={6}>
             <Box>
-              <List title={t("resume_education")} items={itemsEducation} />
+              <List title={t("resume_education")} items={itemsEducation(t)} />
             </Box>
           </Grid>
         </Grid>
